025175ec90997e044a1ea19ea7dfd3274efefdf7
TheChanRProject <garpchoo@gmail.com>
2020-02-05 21:34:23 -0800

    Commiting my changes

--- geospatial_project/src/scripts/geospatial_app.py
+++ geospatial_project/src/scripts/geospatial_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import streamlit as st
 from nearest_restaurants_schools_oop import nearest_restaurants
 from os import getcwd, listdir
+from time import sleep
 
 # Data Path
 data_path = getcwd() + "/geospatial_project/data/csv"
@@ -10,3 +11,13 @@ data_path = getcwd() + "/geospatial_project/data/csv"
 st.title("Geospatial Exploration with Schools")
 
 nr = nearest_restaurants(f"{data_path}/{listdir(data_path)[0]}")
+
+st.write("## Welcome to ChanR Analytic's First Project!")
+
+st.write("## Objectives: ")
+
+st.write("- Transforming Coordinates Into Data via Google Maps API")
+st.write("- Visualizing Data on a Map")
+
+if st.button("Show School Data"):
+    st.write(nr.df)
