098ebdfde1d1bda93de1024d08fe3396af433e0c
Rishov Chatterjee <garpchoo@gmail.com>
2020-02-14 13:29:54 -0800

    adding edits

--- panda_sentiment_project/src/sentiment_project.py
+++ panda_sentiment_project/src/sentiment_project.py
@@ -2,31 +2,45 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import nltk
+import re
+from nltk.tokenize import sent_tokenize, word_tokenize
 from os import getcwd, listdir
+from string import punctuation
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, matthews_corrcoef
-from xgboost import XGBClassifier
 from scikitplot.metrics import plot_confusion_matrix
-from hate_speech_project.src.python.hate_speech_oop import tokenize, HatebaseTwitter
 
 
-hb_path = getcwd() + "/hate_speech_project/data/HatebaseTwitter"
-hb = HatebaseTwitter(hb_path)
-features = hb.features()
+# Borrowing Davidson's Preprocess() Function
+def preprocess(text_string):
+    """
+    Accepts a text string and replaces:
+    1) urls with URLHERE
+    2) lots of whitespace with one instance
+    3) mentions with MENTIONHERE
+    This allows us to get standardized counts of urls and mentions
+    Without caring about specific people mentioned
+    """
+    space_pattern = '\s+'
+    giant_url_regex = ('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|'
+        '[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    mention_regex = '@[\w\-]+'
+    parsed_text = re.sub(space_pattern, ' ', text_string)
+    parsed_text = re.sub(giant_url_regex, 'URLHERE', parsed_text)
+    parsed_text = re.sub(mention_regex, 'MENTIONHERE', parsed_text)
+    #parsed_text = parsed_text.code("utf-8", errors='ignore')
+    return parsed_text
 
-
+# Reading in the Sentiments Data Frame Annotated by CrowdFlower on Data.World
 df = pd.read_csv("https://query.data.world/s/33zqxxshjwehd5xoktqpxyiuyqzedm")
 df.drop("author", axis=1, inplace=True)
 df.head()
 
-print(tokenize(hb.df['tweet'].tolist()[0]))  
-
-# Visualizing the Sentiments
+# Visualizing the Sentiment Counts
 sentiment = df['sentiment']
 x = sentiment.value_counts()
-x.sort_index()
 print(x)
 
 img_path = getcwd() + "/panda_sentiment_project/data_viz"
@@ -41,54 +55,9 @@ plt.xlabel('Sentiments', size=14)
 labels = x.values
 plt.savefig(f"{img_path}/sentiment_counts.png")
 
-# Getting the Dataset with Final Features
-csv_path = getcwd() + "/panda_sentiment_project/data"
-final_df = pd.read_csv(f"{csv_path}/{listdir(csv_path)[0]}")
-final_df.head()
-# Machine Learning
-X = final_df[['compound', 'negative', 'neutral', 'positive']]
-y = final_df['sentiment']
-y.replace("happiness", 1, inplace=True)
-y.replace("sadness", 0, inplace=True)
-X.shape
-y.shape
-X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, stratify=y)
-X_train.shape
-Y_train.shape
+# Preparing the NLP Features
+nltk.download('punkt')
 
-X_train = np.array(X_train)
-Y_train = np.array(Y_train)
-Y_test = np.array(Y_test).reshape(-1, 1)
-model = LogisticRegression(solver='saga',l1_ratio=0.1, penalty='elasticnet')
-model.fit(X_train, Y_train)
-Y_pred = model.predict(X_test)
-
-accuracy_score(Y_test, Y_pred)
-print(classification_report(Y_test, Y_pred))
-
-rf_model = RandomForestClassifier(n_estimators=600, max_depth=5, bootstrap=True)
-rf_model.fit(X_train, Y_train)
-Y_pred = rf_model.predict(X_test)
-
-accuracy_score(Y_test, Y_pred)
-print(classification_report(Y_test, Y_pred))
-
-ada_model = AdaBoostClassifier(n_estimators=400, learning_rate=0.000005)
-ada_model.fit(X_train, Y_train)
-Y_pred = ada_model.predict(X_test)
-
-accuracy_score(Y_test, Y_pred)
-print(classification_report(Y_test, Y_pred))
-
-
-xgb_model = XGBClassifier(n_estimators=400, learning_rate=0.00000005)
-xgb_model.fit(X_train, Y_train)
-
-Y_pred = xgb_model.predict(np.array(X_test))
-
-accuracy_score(Y_pred, Y_test)
-print(classification_report(Y_pred, Y_test))
-
-
-plt.figure(figsize=(10,10))
-plot_confusion_matrix(Y_test, Y_pred)
+# Cleaning, Preprocessing, and Tokenizing Tweets
+sample_sentence = "Hello, my name is Tam."
+word_tokenize(sample_sentence)
