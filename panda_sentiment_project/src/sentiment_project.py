18a74e66870b7fa2772fde0f7f355c036dbce29b
TheChanRProject <garpchoo@gmail.com>
2020-02-13 23:19:57 -0800

    Commiting my changes

--- panda_sentiment_project/src/sentiment_project.py
+++ panda_sentiment_project/src/sentiment_project.py
@@ -3,11 +3,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from os import getcwd, listdir
-from hate_speech_project.src.python.hate_speech_oop import tokenize, HatebaseTwitter
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, matthews_corrcoef
+from xgboost import XGBClassifier
+from scikitplot.metrics import plot_confusion_matrix
 
 df = pd.read_csv("https://query.data.world/s/33zqxxshjwehd5xoktqpxyiuyqzedm")
 df.drop("author", axis=1, inplace=True)
-
+df.head()
+tweet = "Hello, I am sleepy."
+tweet = tokenize(tweet)
+tweet
+#tokens = re.split("[^a-zA-Z]*", tweet.lower())
+tokens = [stemmer.stem(t) for t in tweet.split()]
 # Visualizing the Sentiments
 sentiment = df['sentiment']
 x = sentiment.value_counts()
@@ -25,3 +35,55 @@ plt.ylabel('Count', size=14)
 plt.xlabel('Sentiments', size=14)
 labels = x.values
 plt.savefig(f"{img_path}/sentiment_counts.png")
+
+# Getting the Dataset with Final Features
+csv_path = getcwd() + "/panda_sentiment_project/data"
+final_df = pd.read_csv(f"{csv_path}/{listdir(csv_path)[0]}")
+final_df.head()
+# Machine Learning
+X = final_df[['compound', 'negative', 'neutral', 'positive']]
+y = final_df['sentiment']
+y.replace("happiness", 1, inplace=True)
+y.replace("sadness", 0, inplace=True)
+X.shape
+y.shape
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, stratify=y)
+X_train.shape
+Y_train.shape
+
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)
+Y_test = np.array(Y_test).reshape(-1, 1)
+model = LogisticRegression(solver='saga',l1_ratio=0.1, penalty='elasticnet')
+model.fit(X_train, Y_train)
+Y_pred = model.predict(X_test)
+
+accuracy_score(Y_test, Y_pred)
+print(classification_report(Y_test, Y_pred))
+
+rf_model = RandomForestClassifier(n_estimators=600, max_depth=5, bootstrap=True)
+rf_model.fit(X_train, Y_train)
+Y_pred = rf_model.predict(X_test)
+
+accuracy_score(Y_test, Y_pred)
+print(classification_report(Y_test, Y_pred))
+
+ada_model = AdaBoostClassifier(n_estimators=400, learning_rate=0.000005)
+ada_model.fit(X_train, Y_train)
+Y_pred = ada_model.predict(X_test)
+
+accuracy_score(Y_test, Y_pred)
+print(classification_report(Y_test, Y_pred))
+
+
+xgb_model = XGBClassifier(n_estimators=400, learning_rate=0.00000005)
+xgb_model.fit(X_train, Y_train)
+
+Y_pred = xgb_model.predict(np.array(X_test))
+
+accuracy_score(Y_pred, Y_test)
+print(classification_report(Y_pred, Y_test))
+
+
+plt.figure(figsize=(10,10))
+plot_confusion_matrix(Y_test, Y_pred)
